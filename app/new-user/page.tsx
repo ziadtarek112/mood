aff2fe38d7fbe1ddee372e3bc81ba32a56d36b7e
ziad tarek <66745488+ziadtarek112@users.noreply.github.com>
2024-01-21 21:00:25 +0200

    sign up implementation

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import { prisma } from '@/utils/db';
+import { currentUser } from '@clerk/nextjs'
+import { redirect } from 'next/navigation';
 
-const NewUser = () => {
+const createNewUser = async () => {
+  const user = await currentUser();
+  const match = await prisma.user.findUnique({
+    where: {
+      clerkId: user?.id
+    }
+  })
+
+  if (!match) {
+    const newUser = await prisma.user.create({
+      data: {
+        clerkId: user?.id as string,
+        email: user?.emailAddresses[0].emailAddress as string,
+      }
+    })
+  }
+  redirect('/journal');
+}
+const NewUser = async () => {
+  await createNewUser();
   return (
-    <div>NewUser</div>
+    <div>Loading....</div>
   )
 }
 
